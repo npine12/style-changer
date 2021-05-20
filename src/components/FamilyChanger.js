960b8a94240626f512a1adda2f500a1145e4bf43
Noe Pinedo <noe@MacBook-Pro.local>
2021-05-19 19:24:19 -0600

    redo

--- src/components/FamilyChanger.js
+++ src/components/FamilyChanger.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 export default class FamilyChanger extends Component {
   render() {
     return (
-      <select className="dropDownContainer">
+      <select onChange={(e) => this.props.update(e.target.value)}
+        disabled={this.props.allowEdit === "false" ? true : false} className="dropDownContainer">
         <option value="monospace"> Monospace </option>
         <option value="arial"> Arial </option>
         <option value="courier"> Courier </option>
+        <option value="gothic"> Gothic </option>
       </select>
     );
   }
--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
-export default class ColorChanger extends Component {
+export default class SizeChanger extends Component {
   render() {
     return (
-      <select className="dropDownContainer">
+      <select onChange={(e) => this.props.update(+e.target.value)}
+        disabled={this.props.allowEdit === "false" ? true : false}
+        className="dropDownContainer">
         <option value="12"> 12 </option>
         <option value="13"> 13 </option>
         <option value="14"> 14 </option>
+        <option value="50"> 50 </option>
       </select>
     );
   }
